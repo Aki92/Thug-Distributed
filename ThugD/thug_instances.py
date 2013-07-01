e7a178f53c063fb11e0fde78ff7aa47686f30393
Aki92 <akshit.jiit@gmail.com>
2013-07-01 21:19:59 +0530

    Updated

---
+++ ThugD/thug_instances.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from ThugD.main_server import thugd
+import time
+
+# Making Thug Function to be called as task
+@thugd.task
+def thug(n):
+	time.sleep(n)
+	return "%d Job done" % n
