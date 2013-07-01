58597716a5079ae6881bca0de97b713e45de7fa7
Aki92 <akshit.jiit@gmail.com>
2013-07-01 21:20:44 +0530

    Use to run different type of tasks on running workers

---
+++ run_tasks.py
@@ -0,0 +1,18 @@
+import ThugD.geolocation as geolocation
+from ThugD.thug_instances import thug
+from celery import group
+
+obj = geolocation.FindLocation()
+country = obj.find_country()
+
+# Running single task & getting result back
+res = thug.delay(5).get()
+print(res)
+
+# Running 10 tasks in default queue & getting their results back
+res = group(thug.s(t) for t in xrange(10))().get()
+print(res)
+
+# Running 5 tasks in geolocation based queue & getting their results back
+res = group(thug.apply_async(args=(t,),queue=country).get() for t in xrange(5))
+print(res)
