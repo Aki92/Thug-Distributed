5139363acb848fd3369f0c2c9deb382f27cb3c48
Aki92 <akshit.jiit@gmail.com>
2013-07-25 14:51:17 +0530

    Indentation fixed

--- ThugD/thug_instances.py
+++ ThugD/thug_instances.py
@@ -1,20 +1,9 @@
 from __future__ import absolute_import
 from ThugD.main_server import thugd
-import call_thug
 import time
-import os
 
 # Making Thug Function to be called as task
 @thugd.task
-def thug(url, **options):
-    if not os.getenv('THUG_PROFILE', None):
-        res = call_thug.Thug(url)
-        log = res.analyze(**options)
-    else:
-        import cProfile
-        import pstats
-        cProfile.run('Thug(sys.argv[1:])()', 'countprof')
-        p = pstats.Stats('countprof')
-        res = p.print_stats()
-
-    return log
+def thug(n):
+	time.sleep(n)
+	return "%d Job done" % n
