b5decff24c7696b35c8ac0c1fdb18377e42cadc1
Aki92 <akshit.jiit@gmail.com>
2013-08-19 16:21:03 +0530

    Wrong Place

--- thug_instances.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-from ThugD.main_server import thugd
-import time
-
-# Making Thug Function to be called as task
-@thugd.task
-def thug(n):
-	time.sleep(n)
-	return "%d Job done" % n
