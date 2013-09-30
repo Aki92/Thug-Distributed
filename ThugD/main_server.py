0d86d165269ba7dcde3127561b1bab3f3a203cb0
Akshit Agarwal <akshit.jiit@gmail.com>
2013-09-30 14:38:23 +0530

    Update main_server.py

--- ThugD/main_server.py
+++ ThugD/main_server.py
@@ -10,14 +10,16 @@ thugd = Celery('ThugD.main_server', include=['ThugD.thug_instances'])
 # Using settings from configuration module
 thugd.config_from_object('ThugD.celeryconfig')
 
-# Making GeolocationQueueConfig class to update Queue settings
 class GeolocationQueueConfig(object):
+    """ GeolocationQueueConfig class to update Queue settings """
     def __init__(self):
+        """ Finding country and running required functions """
         obj  = geolocation.FindLocation()
         self.country  = obj.find_country()
         self.update_queue()
     
     def update_queue(self):
+        """ Updating Queue settings """
         country = self.country
         if(country != ''):
             thugd.conf.CELERY_QUEUES = (Queue(country, 
@@ -34,6 +36,7 @@ class GeolocationQueueConfig(object):
                                         routing_key='generic',
                                         durable=True),
                                         )
+# Running Class init function                                        
 GeolocationQueueConfig()
 
 if __name__ == '__main__':
